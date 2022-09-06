7caa9cef203bb254fc165ab996c08e5f0ac187ec
clara gholami <claragholami@gmail.com>
2022-09-06 09:55:29 +0200

    clean data function and score recipes second draft

--- clara.py
+++ clara.py
@@ -20,18 +20,18 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     recipe_cols = ['RecipeId', 'Name', 'CookTime', 'PrepTime', 'TotalTime', 'Description', 'Images',\
               'RecipeCategory', 'RecipeIngredientParts', \
               'AggregatedRating','ReviewCount', 'Calories', 'RecipeInstructions','AuthorName']
-    
+
     rp_df = df[recipe_cols]
 
     # remove missing values transactions
     df = df.drop_duplicates()
     rp_df.CookTime.replace(np.nan, '0M', inplace=True)
-    rp_df.AggregatedRating.replace(np.nan, 'Norating', inplace=True)
+    rp_df.AggregatedRating.replace(np.nan, 0.0, inplace=True)
     rp_df.ReviewCount.replace(np.nan,0, inplace=True)
     rp_df.RecipeCategory.replace(np.nan, 'Others', inplace=True)
     rp_df.Description.replace(np.nan, 'No description', inplace=True)
     rp_df = rp_df.dropna(subset = ['Images'])
-    
+
     #time cleaning
     rp_df['CookTime'] = rp_df['CookTime'].str.replace('PT','')
     rp_df['PrepTime'] = rp_df['PrepTime'].str.replace('PT','')
@@ -41,30 +41,27 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     #reorganize columns
     recipes_cleaned = rp_df.drop(columns=['RecipeId'])
     recipes_cleaned = recipes_cleaned[['Name','AuthorName','AggregatedRating','ReviewCount','Description','RecipeCategory','TotalTime','CookTime','PrepTime','Calories','RecipeIngredientParts','RecipeInstructions','Images']]
-    
+
 
     print("\n✅ data cleaned")
 
     return recipes_cleaned
 
 def score_recipes(user_input, df, best_num):
-    pass
-     '''
+    '''
     user_input: list of strings
     df: our list of recipes
     best_num: number of best matching result to return
     '''
-    df = recipes_cleaned.copy()
-    
-    def score(ingredient_list):
+    df = df.copy()
+
+    def score(ingredient_list):#the score will provide the best related recipes
         score = 0
         for w in user_input:
             if w in ingredient_list:
                 score += 1
         return score
-    
-    df['score'] = df['RecipeIngredientParts'].apply(lambda x: score(x))
-    df = df.sort_values(by='score', ascending=False).iloc[:best_num] #this is sorted bycorresponding score (not only the rating)
-    return df
 
-    
+    df['score'] = df['RecipeIngredientParts'].apply(lambda x: score(x))
+    df = df.sort_values(by=['score','AggregatedRating'], ascending=[False,False]).iloc[:best_num]
+    return df
