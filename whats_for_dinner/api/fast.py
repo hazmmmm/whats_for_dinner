4a21292a649a9d902b8f863205bd71ed36271e55
IbrahimHazm BinAmran <hazmmmm@gmail.com>
2022-09-09 16:14:54 +0900

    updated fast.py

--- whats_for_dinner/api/fast.py
+++ whats_for_dinner/api/fast.py
@@ -4,7 +4,7 @@ import pytz
 import pandas as pd
 
 from whats_for_dinner.ml_logic.registry import load_model
-from whats_for_dinner.ml_logic.preprocessor import preprocess_features
+# from whats_for_dinner.ml_logic.preprocessor import preprocess_features
 # $WIPE_END
 
 from fastapi import FastAPI
@@ -29,7 +29,7 @@ app.add_middleware(
 app.state.model = load_model()
 # $WIPE_END
 
-# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
+# # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
 @app.get("/predict")
 def predict(pickup_datetime: datetime,  # 2013-07-06 17:18:00
             pickup_longitude: float,    # -73.950655
@@ -47,32 +47,31 @@ def predict(pickup_datetime: datetime,  # 2013-07-06 17:18:00
     the parameters of the functions which are all received as strings
     """
 
-    # fixing a value for the key, unused by the model
-    # in the future the key might be removed from the pipeline input
-    key = "2013-07-06 17:18:00.000000119"
+#     # fixing a value for the key, unused by the model
+#     # in the future the key might be removed from the pipeline input
+#     key = "2013-07-06 17:18:00.000000119"
 
-    X_pred = pd.DataFrame(dict(
-        key=[key],  # useless but the pipeline requires it
-        pickup_datetime=[formatted_pickup_datetime],
-        pickup_longitude=[pickup_longitude],
-        pickup_latitude=[pickup_latitude],
-        dropoff_longitude=[dropoff_longitude],
-        dropoff_latitude=[dropoff_latitude],
-        passenger_count=[passenger_count]))
+#     X_pred = pd.DataFrame(dict(
+#         key=[key],  # useless but the pipeline requires it
+#         pickup_datetime=[formatted_pickup_datetime],
+#         pickup_longitude=[pickup_longitude],
+#         pickup_latitude=[pickup_latitude],
+#         dropoff_longitude=[dropoff_longitude],
+#         dropoff_latitude=[dropoff_latitude],
+#         passenger_count=[passenger_count]))
 
-    model = app.state.model
-    X_processed = preprocess_features(X_pred)
-    y_pred = model.predict(X_processed)
+#     model = app.state.model
+#     X_processed = preprocess_features(X_pred)
+#     y_pred = model.predict(X_processed)
 
-    # ⚠️ fastapi only accepts simple python data types as a return value
-    # among which dict, list, str, int, float, bool
-    # in order to be able to convert the api response to json
-    return dict(fare=float(y_pred))
+#     # ⚠️ fastapi only accepts simple python data types as a return value
+#     # among which dict, list, str, int, float, bool
+#     # in order to be able to convert the api response to json
+#     return dict(fare=float(y_pred))
 
+    return
 
 
 @app.get("/")
 def root():
-    # $CHA_BEGIN
     return dict(greeting="Hello")
-    # $CHA_END
