d449251415649dffedcd32a31fd2f62fbdb798c3
clara gholami <claragholami@gmail.com>
2022-09-13 10:23:26 +0200

    changed button to checkbox

--- whats_for_dinner/ml_logic/app.py
+++ whats_for_dinner/ml_logic/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit import session_state
 import numpy as np
 import pandas as pd
 import os
@@ -7,57 +8,58 @@ from whats_for_dinner.data.data import score_recipes
 from whats_for_dinner.ml_logic.main import pred_streamlit
 
 
-
 from PIL import Image
-image1 = Image.open('../../raw_data/food.jpg')
-st.image(image1, caption='copyright Ibrahim Hazm', width=400)
+
+image1 = Image.open("../../raw_data/food.jpg")
+st.image(image1, caption="copyright Ibrahim Hazm", width=400)
 
 
 st.title("What's for dinner :spaghetti:", anchor="title")
 subtitle = '<p style="font-family:sans-serif;color:red;font-weight:bold">Le Wagon - Data Science</p>'
-st.markdown(subtitle,unsafe_allow_html=True)
+st.markdown(subtitle, unsafe_allow_html=True)
 
 
 #'''
-
 
 #TO UPDATE !! This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
 #'''
-#RESUME
-st.markdown('''
-Well, here we go again, grocery time ...:sleepy:
 
-Hey you know what? I'm tired of always eating the same thing, let's change!
-I want to try out this ingredient but don't know what to do with it :thinking_face:
-''')
+# RESUME
+st.header("Find recipe ideas.", anchor="1st step")
+st.markdown(
+    """
+Feeling short on time and short on dinner inspiration? We’ve got the best and delicious dishes to break you out of your weekday recipe rut.
 
-#UPLOAD PICTURE
-st.header("Upload a picture", anchor="1st step")
-user_input = st.file_uploader('')
-#user_input = user_input.read()
+Search by the ingredients themselves by uploading a picture, then add filters like the type of meal and add any dietary restrictions.
+"""
+)
+
+# UPLOAD PICTURE
+
+user_input = st.file_uploader("Upload a picture")
+
+
+if 'key' not in session_state:
+    session_state.key = 0
+
 
 #PREDICTION
-if st.button('predict'):
-    # print is visible in the server output, not in the page
+load= st.checkbox('predict')
+if load:
     st.image(user_input)
     pred = st.write(pred_streamlit(user_input))
 
+
+best_num = int(
+        st.number_input(
+            "How many recipes do you want?", min_value=1, max_value=8, step=1, value=5
+        )
+    )
+
+# NUMBER OF RECIPES
+if st.button("Show me the best recipes"):
+
+     food_output = st.write(score_recipes(pred_streamlit(user_input), best_num))
+
 else:
-    st.write('click me :point_up_2:')
-
-
-#NUMBER OF RECIPES
-best_num = int(st.number_input('How many recipes do you want?', min_value=1, max_value=8, step=1, value=5))
-if st.button('Show me the best recipes'):
-    # print is visible in the server output, not in the page
-    food_output = st.write(score_recipes(pred_streamlit(user_input),best_num))
-
-else:
-    st.write('click me :spaghetti:')
-
-
-
-#csv_path2 = os.path.join('raw_data')
-#recipes_cleaned = pd.read_csv('/home/clara/code/hazmmmm/whats_for_dinner/raw_data/recipes_cleaned.csv')
-#'''display the original dataframe'''
-#st.dataframe(recipes_cleaned.head(4))
+    st.write("click me :spaghetti:")
