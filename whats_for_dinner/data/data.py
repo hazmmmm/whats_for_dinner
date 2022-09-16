b11b5b605c809173c2ebee57bd6f56bd12a8f247
Viktor Chmilenko <v.v.chmilenko@googlemail.com>
2022-09-16 15:22:53 +0200

    Clean Data.py and app.py

--- whats_for_dinner/data/data.py
+++ whats_for_dinner/data/data.py
@@ -4,20 +4,21 @@ import os
 from whats_for_dinner.ml_logic.params import RUN_TYPE
 
 
-#TO UPDATE url = 'https://sample.com/file.csv'
-#recipes_df = pd.read_csv(url)
-#csv_path = os.path.join('raw_data','food_dot_com')
-#recipes_df = pd.read_csv(os.path.join(csv_path, 'recipes.csv'))
+# TO UPDATE url = 'https://sample.com/file.csv'
+# recipes_df = pd.read_csv(url)
+# csv_path = os.path.join('raw_data','food_dot_com')
+# recipes_df = pd.read_csv(os.path.join(csv_path, 'recipes.csv'))
 
-'''
+"""
 if RUN_TYPE == 'local':
     csv_path2 = os.path.join(os.path.expanduser(os.environ.get("LOCAL_DATA_PATH")),'recipes_cleaned.csv')
 if RUN_TYPE == 'docker':
     csv_path2 = ('./whats_for_dinner/data/recipes_cleaned.csv')
-'''
-csv_path2 = ('./whats_for_dinner/data/recipes_cleaned.csv')
+"""
+csv_path2 = "./whats_for_dinner/data/recipes_cleaned.csv"
 
 recipes_cleaned = pd.read_csv(csv_path2)
+
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -27,6 +28,68 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
 
     # remove useless/redundant columns and reorganize
     recipe_cols = [
+        "Name",
+        "AuthorName",
+        "AggregatedRating",
+        "ReviewCount",
+        "Description",
+        "RecipeCategory",
+        "TotalTime",
+        "CookTime",
+        "PrepTime",
+        "Calories",
+        "RecipeIngredientParts",
+        "RecipeInstructions",
+        "Images",
+    ]
+
+    df = df[recipe_cols]
+
+    # remove missing values transactions
+    recipes_cleaned = df.drop_duplicates()
+    recipes_cleaned.CookTime.replace(np.nan, "0M", inplace=True)
+    recipes_cleaned.ReviewCount.replace(np.nan, 0, inplace=True)
+    recipes_cleaned.RecipeCategory.replace(np.nan, "Others", inplace=True)
+    recipes_cleaned.Description.replace(np.nan, "No description", inplace=True)
+    recipes_cleaned = recipes_cleaned.dropna(subset=["Images"])
+
+    # remove
+    recipes_cleaned = recipes_cleaned.dropna(subset=["AggregatedRating"])
+    recipes_cleaned = recipes_cleaned[recipes_cleaned.AggregatedRating > 3]
+
+    # time cleaning
+    recipes_cleaned["CookTime"] = recipes_cleaned["CookTime"].str.replace("PT", "")
+    recipes_cleaned["PrepTime"] = recipes_cleaned["PrepTime"].str.replace("PT", "")
+    recipes_cleaned["TotalTime"] = recipes_cleaned["TotalTime"].str.replace("PT", "")
+
+    print("\n✅ data cleaned")
+
+    return recipes_cleaned
+
+
+def score_recipes(user_input, best_num):
+    """
+    user_input: list of strings
+    df: our list of recipes
+    best_num: number of best matching result to return
+    """
+
+    df = recipes_cleaned
+
+    def score(ingredient_list):  # the score will provide the best related recipes
+        score = 0
+        for w in user_input:
+            if w in ingredient_list:
+                score = 1
+        return score
+
+    df["score"] = df["RecipeIngredientParts"].apply(lambda x: score(x))
+    df = df.sort_values(
+        by=["score", "AggregatedRating"], ascending=[False, False]
+    ).iloc[:best_num]
+
+    return df[
+        [
             "Name",
             "AuthorName",
             "AggregatedRating",
@@ -39,59 +102,8 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
             "Calories",
             "RecipeIngredientParts",
             "RecipeInstructions",
-            "Images",
         ]
-
-    df = df[recipe_cols]
-
-    # remove missing values transactions
-    recipes_cleaned = df.drop_duplicates()
-    recipes_cleaned.CookTime.replace(np.nan, "0M", inplace=True)
-    recipes_cleaned.ReviewCount.replace(np.nan, 0, inplace=True)
-    recipes_cleaned.RecipeCategory.replace(np.nan, "Others", inplace=True)
-    recipes_cleaned.Description.replace(np.nan, "No description", inplace=True)
-    recipes_cleaned = recipes_cleaned.dropna(subset=["Images"])
-
-
-    #remove
-    recipes_cleaned = recipes_cleaned.dropna(subset=["AggregatedRating"])
-    recipes_cleaned = recipes_cleaned[recipes_cleaned.AggregatedRating > 3]
-
-
-    # time cleaning
-    recipes_cleaned["CookTime"] = recipes_cleaned["CookTime"].str.replace("PT", "")
-    recipes_cleaned["PrepTime"] = recipes_cleaned["PrepTime"].str.replace("PT", "")
-    recipes_cleaned["TotalTime"] = recipes_cleaned["TotalTime"].str.replace("PT", "")
-
-
-    print("\n✅ data cleaned")
-
-    return recipes_cleaned
-
-
-def score_recipes(user_input, best_num):
-    '''
-    user_input: list of strings
-    df: our list of recipes
-    best_num: number of best matching result to return
-    '''
-
-    df = recipes_cleaned
-
-    def score(ingredient_list):#the score will provide the best related recipes
-        score = 0
-        for w in user_input:
-            if w in ingredient_list:
-                score += 1
-        return score
-
-    df['score'] = df['RecipeIngredientParts'].apply(lambda x: score(x))
-    df = df.sort_values(by=['score','AggregatedRating'], ascending=[False,False]).iloc[:best_num]
-    return df[['Name','AggregatedRating','ReviewCount','Description','RecipeCategory','TotalTime','Calories']]
-    #return df[['Name","AuthorName","AggregatedRating","ReviewCount","Description","RecipeCategory","TotalTime","CookTime","PrepTime","Calories"]]
-
-
-
+    ]
 
 food_label = {
     0: "apple",
@@ -131,21 +143,3 @@ food_label = {
     34: "turnip",
     35: "watermelon",
 }
-
-
-#df2=score_recipes('tomato',best_num=(int(2)))
-
-#print(score_recipes(user_input=food_label[4], recipes_cleaned,
-#best_num=(int(input("How many recipes you want? ")))))
-
-
-#print(score_recipes(user_input=food_label[4], df=clean_data(recipes_df),
-#best_num=(int(input("How many recipes you want? ")))))
-
-
-
-#print(score_recipes(user_input=input("What should it include? "),
-#best_num=(int(input("How many recipes you want? ")))))
-
-#print(score_recipes('tomato',best_num=(int(2))))
-#print(df2.dtypes)
--- whats_for_dinner/ml_logic/app.py
+++ whats_for_dinner/ml_logic/app.py
@@ -5,14 +5,14 @@ import numpy as np
 import pandas as pd
 import os
 import requests
-#from whats_for_dinner.data.data import score_recipes
-#from whats_for_dinner.ml_logic.main import pred_streamlit
-
-
 from PIL import Image
 
-image1 = Image.open('../../raw_data/food.jpg')
-st.image(image1, caption='copyright Ibrahim Hazm', width=400)
+# from whats_for_dinner.data.data import score_recipes
+# from whats_for_dinner.ml_logic.main import pred_streamlit
+
+
+image1 = Image.open("../../raw_data/food.jpg")
+st.image(image1, caption="copyright Ibrahim Hazm", width=400)
 
 
 st.title("What's for dinner :spaghetti:", anchor="title")
@@ -20,11 +20,9 @@ subtitle = '<p style="font-family:sans-serif;color:red;font-weight:bold">Le Wago
 st.markdown(subtitle, unsafe_allow_html=True)
 
 
-'''
-
- This front queries batch  #991 [what's for dinner model API](https://whatsfordinner-fua6zmtsfa-ew.a.run.app
-)
-'''
+"""
+ This front queries batch  #991 [what's for dinner model API](https://whatsfordinner-fua6zmtsfa-ew.a.run.app)
+"""
 
 # RESUME
 st.header("Find recipe ideas.", anchor="1st step")
@@ -36,50 +34,54 @@ Search by the ingredients themselves by uploading a picture, then add filters li
 """
 )
 
-
 ## API-run case
+predict_uri = "https://whatsfordinner-fua6zmtsfa-ew.a.run.app/predict2"
+recipes_return_uri = ("https://whatsfordinner-fua6zmtsfa-ew.a.run.app/recipe_pull2?recipes_num=5")
 
-predict_uri = 'https://whatsfordinner-fua6zmtsfa-ew.a.run.app/predict2'
-recipes_return_uri = 'https://whatsfordinner-fua6zmtsfa-ew.a.run.app/recipe_pull2?recipes_num=5'
-
-#UPLOAD PICTURE
+# UPLOAD PICTURE
 st.header("Upload a picture", anchor="1st step")
-img_file_buffer = st.file_uploader('')
+img_file_buffer = st.file_uploader("")
 
 classification_result = ""
 
-#PREDICTION
-load= st.button('Get the ingredient!')
+# PREDICTION
+load = st.button("Get the ingredient!")
 
-#Initialize session state
-if "load_state" not in st.session_state :
+# Initialize session state
+if "load_state" not in st.session_state:
     st.session_state.load_state = False
 
 if load or st.session_state.load_state:
     st.session_state.load_state = True
+
     # print is visible in the server output, not in the page
-    st.image(Image.open(img_file_buffer), width=300) # "user_input" type is <class 'streamlit.runtime.uploaded_file_manager.UploadedFile'>
+    st.image(
+        Image.open(img_file_buffer), width=300
+    )  # "user_input" type is <class 'streamlit.runtime.uploaded_file_manager.UploadedFile'>
     img_bytes = img_file_buffer.getvalue()
-    response = requests.post(predict_uri, files={'img': img_bytes})
+    response = requests.post(predict_uri, files={"img": img_bytes})
     classification_result = response.json()
     pred = classification_result[0]
 
-    st.caption(f'your ingredient is: {pred}')
+    st.caption(f"your ingredient is: {pred}")
 
+# NUMBER OF RECIPES
+best_num = int(
+    st.number_input(
+        "How many recipes do you want?", min_value=1, max_value=8, step=1, value=5
+    )
+)
 
-
-#NUMBER OF RECIPES
-best_num = int(st.number_input('How many recipes do you want?', min_value=1, max_value=8, step=1, value=5))
-if st.button('Show me the best recipes!'):
+if st.button("Show me the best recipes!"):
     params = dict(recipes_num=best_num)
     response = requests.post(recipes_return_uri, params=params)
-    output=response.json()
-    food_output = output['Name']
+    output = response.json()
+    food_output = output["Name"]
     for value in food_output.values():
         st.write(value)
 
-    #testing to display review count
-    revcount=output['ReviewCount']
+    # testing to display review count
+    revcount = output["ReviewCount"]
     st.write(revcount["250"])
-    #variable avec filters
-    #checkboxs -total time etc...)
+    # variable avec filters
+    # checkboxs -total time etc...)
