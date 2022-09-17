ed56ac12ac1171af00bd244fcb968493bb6c4496
IbrahimHazm BinAmran <hazmmmm@gmail.com>
2022-09-17 16:29:44 +0900

    cleanup fast.py

--- whats_for_dinner/api/fast.py
+++ whats_for_dinner/api/fast.py
@@ -1,20 +1,11 @@
-from datetime import datetime
-import pytz
-import pandas as pd
-import io
-import sys
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 from whats_for_dinner.ml_logic.registry import load_model
 from whats_for_dinner.ml_logic.main import pred_streamlit
 from whats_for_dinner.data.data import score_recipes
 
-# from whats_for_dinner.ml_logic.preprocessor import preprocess_features
-
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-
 app = FastAPI()
-
 
 # for development purposes
 app.add_middleware(
@@ -25,10 +16,8 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-
 # 💡 Preload the model to accelerate the predictions
 app.state.model = load_model()
-
 
 @app.get("/")
 def index():
@@ -40,12 +29,6 @@ async def classification(img: UploadFile=File(...)):
     """
     receive image from user and return a predicted class
     """
-    # initial type is <class 'starlette.datastructures.UploadFile'>
-
-
-
-    # #contents = await img.read()
-
     # ### put images in a folder, "folder"
     # folder = ""
     # y_pred = pred(folder)
@@ -59,21 +42,10 @@ async def classification(img: UploadFile=File(...)):
     if not extension:
         return "Image must be .jpg, .jpeg or .png format!"
 
-    # prediction = pred_streamlit(await img.read())
-
-    # prediction = pred_docker(await img.read())
-    print(f"img (straight from initial Upload) type: {type(img)}")
-
     img = img.file.read()
-    # type after img.file.read() is <class 'bytes'>
-
-    # img2 = img.read() ## results in TypeError: a bytes-like object is required, not 'coroutine' in main.py line 163
-    # type after img.file.read() is <class 'coroutine'>
-
-    # image = Image.open(io.BytesIO(image)).convert('RGB')
-
     predicted_class = pred_streamlit(img)
     app.state.predicted_class = predicted_class
+
     return predicted_class
 
 
@@ -107,5 +79,4 @@ async def recipes2(recipes_num: int):
     receive number of recipes desired by the user, and return
     """
     food_output = score_recipes(user_input=app.state.predicted_class,best_num=(int(recipes_num)))
-    print(food_output)
     return food_output
