6db59261ce80123c06b76b7ba4234e639dfb935a
IbrahimHazm BinAmran <hazmmmm@gmail.com>
2022-09-17 20:43:27 +0900

    cleanup app.py

--- whats_for_dinner/ml_logic/app.py
+++ whats_for_dinner/ml_logic/app.py
@@ -1,87 +1,149 @@
-from warnings import filters
 import streamlit as st
-from streamlit import session_state
-import numpy as np
 import pandas as pd
-import os
 import requests
 from PIL import Image
 
-# from whats_for_dinner.data.data import score_recipes
-# from whats_for_dinner.ml_logic.main import pred_streamlit
+def retrieve_instructions(cell):
+    '''
+    Cleans up the text in instructinos from the food df
+    '''
+    cell = cell.lstrip("c(")
+    cell = cell.rstrip(")")
+    cell = [line for line in [line.strip() for line in cell.split("\", \"")] if line]
+    cell = [sentence.strip("\"") for sentence in cell]
+    cell = '  \n  \n'.join(cell)
+    return cell
 
 
-image1 = Image.open("../../raw_data/food.jpg")
-st.image(image1, caption="copyright Ibrahim Hazm", width=400)
+def retrieve_ingredients(cell):
+    '''
+    Cleans up the text in ingredients from the food df
+    '''
+    cell = cell.lstrip("c(")
+    cell = cell.rstrip(")")
+    cell = [line for line in [line.strip() for line in cell.split("\", \"")] if line]
+    cell = [word.strip("\"") for word in cell]
+    cell = '  \n  \n'.join(cell)
+    return cell
 
 
-st.title("What's for dinner :spaghetti:", anchor="title")
-subtitle = '<p style="font-family:sans-serif;color:red;font-weight:bold">Le Wagon - Data Science</p>'
+image1 = Image.open('../../raw_data/food.jpg')
+st.image(image1, caption='copyright Ibrahim Hazm', width=400)
+
+st.title("What's for Dinner? :spaghetti:", anchor="title")
+subtitle = '<p style="font-family:sans-serif;color:red;font-weight:bold">Le Wagon - Data Science: Batch #991</p>'
 st.markdown(subtitle, unsafe_allow_html=True)
-
-
-"""
- This front queries batch  #991 [what's for dinner model API](https://whatsfordinner-fua6zmtsfa-ew.a.run.app)
-"""
+st.write("#")
 
 # RESUME
 st.header("Find recipe ideas.", anchor="1st step")
 st.markdown(
     """
-Feeling short on time and short on dinner inspiration? We’ve got the best and delicious dishes to break you out of your weekday recipe rut.
+Short on **time**? ⏱
 
-Search by the ingredients themselves by uploading a picture, then add filters like the type of meal and add any dietary restrictions.
+Not feeling **inspired**? 🧐
+
+We've got the best and most delicious dishes to break you out of your weekday recipe rut.
+
 """
 )
-
 ## API-run case
-predict_uri = "https://whatsfordinner-fua6zmtsfa-ew.a.run.app/predict2"
-recipes_return_uri = ("https://whatsfordinner-fua6zmtsfa-ew.a.run.app/recipe_pull2?recipes_num=5")
 
-# UPLOAD PICTURE
-st.header("Upload a picture", anchor="1st step")
-img_file_buffer = st.file_uploader("")
+predict_uri = 'https://whatsfordinnernew-fua6zmtsfa-ew.a.run.app/predict2'
+recipes_return_uri = 'https://whatsfordinnernew-fua6zmtsfa-ew.a.run.app/recipe_pull2'
+
+st.write("#")
+
+
+#UPLOAD PICTURE
+st.header("Take a photo of an ingredient and upload it.", anchor="1st step")
+img_file_buffer = st.file_uploader('')
 
 classification_result = ""
+st.write("#####")
 
-# PREDICTION
-load = st.button("Get the ingredient!")
 
-# Initialize session state
-if "load_state" not in st.session_state:
-    st.session_state.load_state = False
+if "button1" not in st.session_state:
+    st.session_state["button1"] = False
 
-if load or st.session_state.load_state:
-    st.session_state.load_state = True
+if "button2" not in st.session_state:
+    st.session_state["button2"] = False
 
-    # print is visible in the server output, not in the page
-    st.image(
-        Image.open(img_file_buffer), width=300
-    )  # "user_input" type is <class 'streamlit.runtime.uploaded_file_manager.UploadedFile'>
+if "button3" not in st.session_state:
+    st.session_state["button3"] = False
+
+
+#PREDICTION
+if st.button('Get the ingredient!'):
+    st.session_state["button1"] = not st.session_state["button1"]
+
+if st.session_state["button1"]:
+    st.image(Image.open(img_file_buffer), width=300) # "user_input" type is <class 'streamlit.runtime.uploaded_file_manager.UploadedFile'>
     img_bytes = img_file_buffer.getvalue()
-    response = requests.post(predict_uri, files={"img": img_bytes})
+    response = requests.post(predict_uri, files={'img': img_bytes}, timeout=30)
     classification_result = response.json()
     pred = classification_result[0]
 
-    st.caption(f"your ingredient is: {pred}")
+    st.header(f"It's {pred}!")
+    st.write("#")
 
-# NUMBER OF RECIPES
-best_num = int(
-    st.number_input(
-        "How many recipes do you want?", min_value=1, max_value=8, step=1, value=5
-    )
-)
+    st.header('Find a recipe.')
 
-if st.button("Show me the best recipes!"):
+
+    #NUMBER OF RECIPES
+    best_num = int(st.number_input('How many recipes are you looking for?', min_value=1, max_value=8, step=1))
+
+    # if st.button('Show me the best recipes!'):
+
+    if st.button('Show me the best recipes!'):
+        st.session_state["button2"] = not st.session_state["button2"]
+
+
+if st.session_state["button1"] and st.session_state["button2"]:
     params = dict(recipes_num=best_num)
-    response = requests.post(recipes_return_uri, params=params)
-    output = response.json()
-    food_output = output["Name"]
-    for value in food_output.values():
-        st.write(value)
+    response = requests.post(recipes_return_uri, params=params, timeout=30)
+    output=response.json()
+    food_df = pd.DataFrame.from_dict(output).reset_index()
 
-    # testing to display review count
-    revcount = output["ReviewCount"]
-    st.write(revcount["250"])
-    # variable avec filters
-    # checkboxs -total time etc...)
+    col1, col2 = st.columns(2)
+    col1.subheader('Title')
+    col2.subheader('Description')
+
+    for index, row in food_df.iterrows():
+        col1, col2 = st.columns(2)
+        col1.write(f"{str(index + 1)}. {row['Name']}")
+        col2.write(row['Description'])
+    st.write("###")
+
+    recipe_choice = int(st.number_input('Which recipe would you like to make?', min_value=1, max_value=food_df.shape[0]+1, step=1))
+
+    if st.button("Let's make it!"):
+        st.session_state["button3"] = not st.session_state["button3"]
+
+
+if st.session_state["button3"]:
+    st.markdown(
+    """
+    ### Ingredients
+    """
+    )
+    st.write(retrieve_ingredients(food_df.iloc[recipe_choice - 1]['RecipeIngredientParts']))
+    st.markdown(
+    """
+    ### Instructions
+    """
+    )
+    st.write(retrieve_instructions(food_df.iloc[recipe_choice - 1]['RecipeInstructions']))
+
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+
+
+'''
+This front-end queries the [what's for dinner API](https://dinner-fua6zmtsfa-ew.a.run.app/) built by batch #991 of the Le Wagon Data Science bootcamp.
+'''
